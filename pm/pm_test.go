053cf70b9133a525d368d3dc2216fc8697bd4d68
yra <ymz.ncnk@outlook.com>
2023-10-27 01:29:25 +0300

    Add pm package

---
+++ pm/pm_test.go
@@ -0,0 +1,460 @@
+package pm
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	com "github.com/mus-format/common-go"
+	com_testdata "github.com/mus-format/common-go/testdata"
+	muss "github.com/mus-format/mus-stream-go"
+	"github.com/mus-format/mus-stream-go/ord"
+	"github.com/mus-format/mus-stream-go/testdata"
+	"github.com/mus-format/mus-stream-go/testdata/mock"
+	"github.com/mus-format/mus-stream-go/varint"
+	"github.com/ymz-ncnk/mok"
+)
+
+func MarshalPointerMappingStruct(v com_testdata.PointerMappingStruct,
+	w muss.Writer) (n int, err error) {
+	mp := com.NewPtrMap()
+	n, err = MarshalPtr[int](v.A1, muss.MarshallerFn[int](varint.MarshalInt),
+		mp,
+		w)
+	if err != nil {
+		return
+	}
+	var n1 int
+	n1, err = MarshalPtr[int](v.A2, muss.MarshallerFn[int](varint.MarshalInt),
+		mp,
+		w)
+	n += n1
+	if err != nil {
+		return
+	}
+	n1, err = MarshalPtr[int](v.B1, muss.MarshallerFn[int](varint.MarshalInt),
+		mp,
+		w)
+	n += n1
+	if err != nil {
+		return
+	}
+	n1, err = MarshalPtr[int](v.B2, muss.MarshallerFn[int](varint.MarshalInt),
+		mp,
+		w)
+	n += n1
+	if err != nil {
+		return
+	}
+	n1, err = MarshalPtr[string](v.C1, muss.MarshallerFn[string](ord.MarshalString),
+		mp,
+		w)
+	n += n1
+	if err != nil {
+		return
+	}
+	n1, err = MarshalPtr[string](v.C2, muss.MarshallerFn[string](ord.MarshalString),
+		mp,
+		w)
+	n += n1
+	return
+}
+
+func UnmarshalPointerMappingStruct(r muss.Reader) (
+	v com_testdata.PointerMappingStruct, n int, err error) {
+	mp := com.NewReversePtrMap()
+	v.A1, n, err = UnmarshalPtr[int](muss.UnmarshallerFn[int](varint.UnmarshalInt),
+		mp,
+		r)
+	if err != nil {
+		return
+	}
+	var n1 int
+	v.A2, n1, err = UnmarshalPtr[int](muss.UnmarshallerFn[int](varint.UnmarshalInt),
+		mp,
+		r)
+	n += n1
+	if err != nil {
+		return
+	}
+	v.B1, n1, err = UnmarshalPtr[int](muss.UnmarshallerFn[int](varint.UnmarshalInt),
+		mp,
+		r)
+	n += n1
+	if err != nil {
+		return
+	}
+	v.B2, n1, err = UnmarshalPtr[int](muss.UnmarshallerFn[int](varint.UnmarshalInt),
+		mp,
+		r)
+	n += n1
+	if err != nil {
+		return
+	}
+	v.C1, n1, err = UnmarshalPtr[string](
+		muss.UnmarshallerFn[string](ord.UnmarshalString),
+		mp,
+		r)
+	n += n1
+	if err != nil {
+		return
+	}
+	v.C2, n1, err = UnmarshalPtr[string](
+		muss.UnmarshallerFn[string](ord.UnmarshalString),
+		mp,
+		r)
+	n += n1
+	return
+}
+
+func SizePointerMappingStruct(v com_testdata.PointerMappingStruct) (size int) {
+	mp := com.NewPtrMap()
+	size = SizePtr[int](v.A1, muss.SizerFn[int](varint.SizeInt), mp)
+	size += SizePtr[int](v.A2, muss.SizerFn[int](varint.SizeInt), mp)
+	size += SizePtr[int](v.B1, muss.SizerFn[int](varint.SizeInt), mp)
+	size += SizePtr[int](v.B2, muss.SizerFn[int](varint.SizeInt), mp)
+	size += SizePtr[string](v.C1, muss.SizerFn[string](ord.SizeString), mp)
+	return size + SizePtr[string](v.C2, muss.SizerFn[string](ord.SizeString),
+		mp)
+}
+
+func SkipPointerMappingStruct(r muss.Reader) (n int, err error) {
+	mp := com.NewReversePtrMap()
+	n, err = SkipPtr(muss.SkipperFn(varint.SkipInt), mp, r)
+	if err != nil {
+		return
+	}
+	var n1 int
+	n1, err = SkipPtr(muss.SkipperFn(varint.SkipInt), mp, r)
+	n += n1
+	if err != nil {
+		return
+	}
+	n1, err = SkipPtr(muss.SkipperFn(varint.SkipInt), mp, r)
+	n += n1
+	if err != nil {
+		return
+	}
+	n1, err = SkipPtr(muss.SkipperFn(varint.SkipInt), mp, r)
+	n += n1
+	if err != nil {
+		return
+	}
+	n1, err = SkipPtr(muss.SkipperFn(ord.SkipString), mp, r)
+	n += n1
+	if err != nil {
+		return
+	}
+	n1, err = SkipPtr(muss.SkipperFn(ord.SkipString), mp, r)
+	n += n1
+	return
+}
+
+func TestPM(t *testing.T) {
+
+	t.Run("All MarshalPtr, UnmarshalPtr, SizePtr, SkipPtr functions must work correctly",
+		func(t *testing.T) {
+			var (
+				m = muss.MarshallerFn[com_testdata.PointerMappingStruct](
+					MarshalPointerMappingStruct)
+				u = muss.UnmarshallerFn[com_testdata.PointerMappingStruct](
+					UnmarshalPointerMappingStruct)
+				s = muss.SizerFn[com_testdata.PointerMappingStruct](
+					SizePointerMappingStruct)
+				sk = muss.SkipperFn(SkipPointerMappingStruct)
+			)
+			testdata.Test[com_testdata.PointerMappingStruct](
+				com_testdata.MakePointerMappingTestStruct(), m, u, s, t)
+			testdata.TestSkip[com_testdata.PointerMappingStruct](
+				com_testdata.MakePointerMappingTestStruct(), m, sk, s, t)
+		})
+
+	t.Run("MarshalPtr should be able to marshal the nil pointer", func(t *testing.T) {
+		var (
+			wantN         = 1
+			wantErr error = nil
+			mp            = com.NewPtrMap()
+			w             = mock.NewWriter().RegisterWriteByte(
+				func(c byte) (err error) {
+					if c != byte(com.Nil) {
+						err = fmt.Errorf("unexpected byte, want '%v' actual '%v'", com.Nil,
+							c)
+					}
+					return
+				},
+			)
+			mocks  = []*mok.Mock{w.Mock}
+			n, err = MarshalPtr[int](nil, nil, mp, w)
+		)
+		testdata.TestMarshalResults(wantN, n, wantErr, err, mocks, t)
+	})
+
+	t.Run("If marshal of the pointer Nil flag fails with an error, MarshalPtr should return it",
+		func(t *testing.T) {
+			var (
+				wantN         = 0
+				wantErr error = errors.New("pointer Nil flag marshal error")
+				mp            = com.NewPtrMap()
+				w             = mock.NewWriter().RegisterWriteByte(
+					func(c byte) (err error) {
+						return wantErr
+					},
+				)
+				mocks  = []*mok.Mock{w.Mock}
+				n, err = MarshalPtr[int](nil, nil, mp, w)
+			)
+			testdata.TestMarshalResults(wantN, n, wantErr, err, mocks, t)
+		})
+
+	t.Run("If marshal of the pointer Mapping flag fails with an error, MarshalPtr should return it",
+		func(t *testing.T) {
+			var (
+				wantN         = 0
+				wantErr error = errors.New("pointer Mapping flag marshal error")
+				mp            = com.NewPtrMap()
+				w             = mock.NewWriter().RegisterWriteByte(
+					func(c byte) (err error) {
+						return wantErr
+					},
+				)
+				num    = 2
+				mocks  = []*mok.Mock{w.Mock}
+				n, err = MarshalPtr[int](&num, nil, mp, w)
+			)
+			testdata.TestMarshalResults(wantN, n, wantErr, err, mocks, t)
+		})
+
+	t.Run("If marshal of the pointer id fails with an error, MarshalPtr should return it",
+		func(t *testing.T) {
+			var (
+				wantN         = 1
+				wantErr error = errors.New("pointer id marshal error")
+				mp            = com.NewPtrMap()
+				w             = mock.NewWriter().RegisterWriteByte(
+					func(c byte) (err error) {
+						return nil
+					},
+				).RegisterWriteByte(
+					func(c byte) error {
+						return wantErr
+					},
+				)
+				num    = 2
+				mocks  = []*mok.Mock{w.Mock}
+				n, err = MarshalPtr[int](&num, nil, mp, w)
+			)
+			testdata.TestMarshalResults(wantN, n, wantErr, err, mocks, t)
+		})
+
+	t.Run("If Marshaller fails with an error, MarshalPtr should return it",
+		func(t *testing.T) {
+			var (
+				wantN         = 3
+				wantErr error = errors.New("marshaller error")
+				mp            = com.NewPtrMap()
+				w             = mock.NewWriter().RegisterWriteByte(
+					func(c byte) (err error) {
+						return nil
+					},
+				).RegisterWriteByte(
+					func(c byte) error {
+						return nil
+					},
+				)
+				m = mock.NewMarshaller[int]().RegisterMarshalMUS(
+					func(t int, w muss.Writer) (n int, err error) {
+						return 1, wantErr
+					},
+				)
+				num    = 2
+				mocks  = []*mok.Mock{w.Mock}
+				n, err = MarshalPtr[int](&num, m, mp, w)
+			)
+			testdata.TestMarshalResults(wantN, n, wantErr, err, mocks, t)
+		})
+
+	t.Run("If unmarshal of the pointer flag fails with an error, UnmarshalPtr should return it",
+		func(t *testing.T) {
+			var (
+				wantV   *int = nil
+				wantN        = 0
+				wantErr      = errors.New("pointer flag unmarshal error")
+				r            = mock.NewReader().RegisterReadByte(
+					func() (b byte, err error) {
+						err = wantErr
+						return
+					},
+				)
+				mocks     = []*mok.Mock{r.Mock}
+				v, n, err = UnmarshalPtr[int](nil, com.ReversePtrMap{}, r)
+			)
+			com_testdata.TestUnmarshalResults[*int](wantV, v, wantN, n, wantErr, err,
+				mocks,
+				t)
+		})
+
+	t.Run("If unmarshal of the pointer id fails with an error, UnmarshalPtr should return it",
+		func(t *testing.T) {
+			var (
+				wantV   *int = nil
+				wantN        = 1
+				wantErr      = errors.New("pointer id unmarshal error")
+				r            = mock.NewReader().RegisterReadByte(
+					func() (b byte, err error) {
+						b = byte(com.Mapping)
+						return
+					},
+				).RegisterReadByte(
+					func() (b byte, err error) {
+						err = wantErr
+						return
+					},
+				)
+				mocks     = []*mok.Mock{r.Mock}
+				v, n, err = UnmarshalPtr[int](nil, com.ReversePtrMap{}, r)
+			)
+			com_testdata.TestUnmarshalResults[*int](wantV, v, wantN, n, wantErr, err,
+				mocks,
+				t)
+		})
+
+	t.Run("If Unmarshaller fails with an error, UnmarshalPtr should return it",
+		func(t *testing.T) {
+			var (
+				wantV   *int = nil
+				wantN        = 3
+				wantErr      = errors.New("Unmarshaller error")
+				r            = mock.NewReader().RegisterReadByte(
+					func() (b byte, err error) {
+						b = byte(com.Mapping)
+						return
+					},
+				).RegisterReadByte(
+					func() (b byte, err error) {
+						b = 1
+						return
+					},
+				)
+				u = mock.NewUnmarshaller[int]().RegisterUnmarshalMUS(
+					func(r muss.Reader) (t int, n int, err error) {
+						n = 1
+						err = wantErr
+						return
+					},
+				)
+				mp        = com.NewReversePtrMap()
+				mocks     = []*mok.Mock{r.Mock}
+				v, n, err = UnmarshalPtr[int](u, mp, r)
+			)
+			com_testdata.TestUnmarshalResults[*int](wantV, v, wantN, n, wantErr, err,
+				mocks,
+				t)
+		})
+
+	t.Run("UnmarshalPtr should fail with com.ErrWrongFormat if meets unknown pointer flag",
+		func(t *testing.T) {
+			var (
+				wantV   *int = nil
+				wantN        = 0
+				wantErr      = com.ErrWrongFormat
+				r            = mock.NewReader().RegisterReadByte(
+					func() (b byte, err error) {
+						b = byte(com.Mapping) + 1
+						return
+					},
+				)
+				mocks     = []*mok.Mock{r.Mock}
+				v, n, err = UnmarshalPtr[int](nil, com.ReversePtrMap{}, r)
+			)
+			com_testdata.TestUnmarshalResults[*int](wantV, v, wantN, n, wantErr, err,
+				mocks,
+				t)
+		})
+
+	t.Run("If unmarshal of the pointer flag fails with an error, SkipPtr should return it",
+		func(t *testing.T) {
+			var (
+				wantN   = 0
+				wantErr = errors.New("pointer flag unmarshal error")
+				r       = mock.NewReader().RegisterReadByte(
+					func() (b byte, err error) {
+						err = wantErr
+						return
+					},
+				)
+				mocks  = []*mok.Mock{r.Mock}
+				n, err = SkipPtr(nil, com.ReversePtrMap{}, r)
+			)
+			com_testdata.TestSkipResults(wantN, n, wantErr, err, mocks, t)
+		})
+
+	t.Run("If unmarshal of the pointer id fails with an error, SkipPtr should return it",
+		func(t *testing.T) {
+			var (
+				wantN   = 1
+				wantErr = errors.New("pointer id unmarshal error")
+				r       = mock.NewReader().RegisterReadByte(
+					func() (b byte, err error) {
+						b = byte(com.Mapping)
+						return
+					},
+				).RegisterReadByte(
+					func() (b byte, err error) {
+						err = wantErr
+						return
+					},
+				)
+				mocks  = []*mok.Mock{r.Mock}
+				n, err = SkipPtr(nil, com.ReversePtrMap{}, r)
+			)
+			com_testdata.TestSkipResults(wantN, n, wantErr, err, mocks, t)
+		})
+
+	t.Run("If Skipper fails with an error, SkipPtr should return it",
+		func(t *testing.T) {
+			var (
+				wantN   = 3
+				wantErr = errors.New("Skipper error")
+				r       = mock.NewReader().RegisterReadByte(
+					func() (b byte, err error) {
+						b = byte(com.Mapping)
+						return
+					},
+				).RegisterReadByte(
+					func() (b byte, err error) {
+						b = 1
+						return
+					},
+				)
+				sk = mock.NewSkipper().RegisterSkipMUS(
+					func(r muss.Reader) (n int, err error) {
+						n = 1
+						err = wantErr
+						return
+					},
+				)
+				mp     = com.NewReversePtrMap()
+				mocks  = []*mok.Mock{r.Mock}
+				n, err = SkipPtr(sk, mp, r)
+			)
+			com_testdata.TestSkipResults(wantN, n, wantErr, err, mocks, t)
+		})
+
+	t.Run("SkipPtr should fail with com.ErrWrongFormat if meets unknown pointer flag",
+		func(t *testing.T) {
+			var (
+				wantN   = 0
+				wantErr = com.ErrWrongFormat
+				r       = mock.NewReader().RegisterReadByte(
+					func() (b byte, err error) {
+						b = byte(com.Mapping) + 1
+						return
+					},
+				)
+				mocks  = []*mok.Mock{r.Mock}
+				n, err = SkipPtr(nil, com.ReversePtrMap{}, r)
+			)
+			com_testdata.TestSkipResults(wantN, n, wantErr, err, mocks, t)
+		})
+
+}
---
+++ pm/ptr.go
@@ -0,0 +1,170 @@
+package pm
+
+import (
+	"unsafe"
+
+	com "github.com/mus-format/common-go"
+	muss "github.com/mus-format/mus-stream-go"
+	"github.com/mus-format/mus-stream-go/varint"
+)
+
+// MarshalPtr fills bs with the MUS encoding of a pointer.
+//
+// The m argument specifies the Marshaller for the pointer base type.
+//
+// Returns the number of used bytes. It will panic if receives too small bs.
+func MarshalPtr[T any](v *T, m muss.Marshaller[T], mp *com.PtrMap,
+	w muss.Writer,
+) (n int, err error) {
+	if v == nil {
+		err = w.WriteByte(byte(com.Nil))
+		if err != nil {
+			return
+		}
+		n++
+		return
+	}
+	err = w.WriteByte(byte(com.Mapping))
+	if err != nil {
+		return
+	}
+	n++
+	id, newOne := maptr(unsafe.Pointer(v), mp)
+	var n1 int
+	n1, err = varint.MarshalInt(id, w)
+	n += n1
+	if err != nil {
+		return
+	}
+	if newOne {
+		n1, err = m.MarshalMUS(*v, w)
+		n += n1
+	}
+	return
+}
+
+// UnmarshalPtr parses a MUS-encoded pointer from bs.
+//
+// The u argument specifies the Unmarshaller for the base pointer type.
+//
+// In addition to the pointer, returns the number of used bytes and one of the
+// mus.ErrTooSmallByteSlice, com.ErrWrongFormat or Unarshaller errors.
+func UnmarshalPtr[T any](u muss.Unmarshaller[T], mp com.ReversePtrMap,
+	r muss.Reader,
+) (v *T, n int, err error) {
+	b, err := r.ReadByte()
+	if err != nil {
+		return
+	}
+	switch b {
+	case byte(com.Nil):
+		n = 1
+		return
+	case byte(com.Mapping):
+		var (
+			n1 int
+			id int
+		)
+		n = 1
+		id, n1, err = varint.UnmarshalInt(r)
+		n += n1
+		if err != nil {
+			return
+		}
+		ptr, _ := mp.Get(id)
+		if ptr == nil {
+			v, n1, err = unmarshalData[T](id, u, mp, r)
+			n += n1
+		} else {
+			v = (*T)(ptr)
+		}
+	default:
+		err = com.ErrWrongFormat
+	}
+	return
+}
+
+// SizePtr returns the size of a MUS-encoded pointer.
+//
+// The s argument specifies the Sizer for the pointer base type.
+func SizePtr[T any](v *T, s muss.Sizer[T], mp *com.PtrMap) (size int) {
+	size = 1
+	if v != nil {
+		id, newOne := maptr(unsafe.Pointer(v), mp)
+		size += varint.SizeInt(id)
+		if newOne {
+			return size + s.SizeMUS(*v)
+		} else {
+			return
+		}
+	}
+	return
+}
+
+// SkipPtr skips a MUS-encoded pointer.
+//
+// The sk argument specifies the Skipper for the pointer base type.
+//
+// Returns the number of skiped bytes and one of the mus.ErrTooSmallByteSlice,
+// com.ErrWrongFormat or Skipper errors.
+func SkipPtr(sk muss.Skipper, mp com.ReversePtrMap, r muss.Reader) (n int,
+	err error) {
+	b, err := r.ReadByte()
+	if err != nil {
+		return
+	}
+	switch b {
+	case byte(com.Nil):
+		n = 1
+	case byte(com.Mapping):
+		n = 1
+		var (
+			id int
+			n1 int
+		)
+		id, n1, err = varint.UnmarshalInt(r)
+		n += n1
+		if err != nil {
+			return
+		}
+		_, pst := mp.Get(id)
+		if !pst {
+			n1, err = sk.SkipMUS(r)
+			n += n1
+			if err != nil {
+				return
+			}
+			mp.Put(id, nil)
+		}
+	default:
+		err = com.ErrWrongFormat
+	}
+	return
+}
+
+func unmarshalData[T any](id int, u muss.Unmarshaller[T],
+	mp com.ReversePtrMap,
+	r muss.Reader,
+) (v *T, n int, err error) {
+	var (
+		k  T
+		n1 int
+	)
+	mp.Put(id, unsafe.Pointer(&k))
+	k, n1, err = u.UnmarshalMUS(r)
+	n += n1
+	if err != nil {
+		return
+	}
+	v = &k
+	return
+}
+
+func maptr(ptr unsafe.Pointer, mp *com.PtrMap) (id int, newOne bool) {
+	id, pst := mp.Get(ptr)
+	if !pst {
+		id = mp.Put(ptr)
+		newOne = true
+	}
+	return
+}
